077283e731b45b0cd40d98787274af22a3a58a53
Yuya Minami <yuya373@me.com>
2015-01-29 01:01:53 +0900

    initial commit

---
+++ metadata.rb
@@ -0,0 +1,8 @@
+name             'hello-chef'
+maintainer       'The Authors'
+maintainer_email 'you@example.com'
+license          'all_rights'
+description      'Installs/Configures hello-chef'
+long_description 'Installs/Configures hello-chef'
+version          '0.1.0'
+
