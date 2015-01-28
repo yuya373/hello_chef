ccf6cf704afd253f653333115bf193dbc5b7c0e1
Yuya Minami <yuya373@me.com>
2015-01-29 01:07:00 +0900

    add ruby recipe

---
+++ recipes/ruby.rb
@@ -0,0 +1,60 @@
+%w(git build-essential libssl-dev libffi-dev).each do |package|
+  package package
+end
+
+git "#{node.home_dir}/.rbenv" do
+  user node.user
+  repository "https://github.com/sstephenson/rbenv.git"
+end
+
+bash 'add rbenv path' do
+  not_if "cat #{node.home_dir}/.bash_profile | grep '/home/vagrant/.rbenv/bin'"
+  code <<-EOH
+  echo 'export PATH=\$HOME/.rbenv/bin:\$PATH' >> #{node.home_dir}/.bash_profile
+  EOH
+end
+
+bash 'add rbenv init' do
+  not_if "cat #{node.home_dir}/.bash_profile | grep 'rbenv init -'"
+  code <<-EOH
+  echo 'eval "$(rbenv init -)"' >> #{node.home_dir}/.bash_profile
+  EOH
+end
+
+bash 'source .bash_profile' do
+  user node.user
+  cwd node.home_dir
+  code 'source .bash_profile'
+end
+
+file "#{node.home_dir}/.bash_profile" do
+  owner node.user
+  group node.group
+  mode 0775
+end
+
+%W(#{node.home_dir}/.rbenv #{node.home_dir}/.rbenv/plugins #{node.home_dir}/.rbenv/plugins/ruby-build).each do |dir|
+  directory dir do
+    owner node.user
+    group node.group
+    mode 0775
+  end
+end
+
+git "#{node.home_dir}/.rbenv/plugins/ruby-build" do
+  user node.user
+  repository "https://github.com/sstephenson/ruby-build.git"
+end
+
+bash 'install ruby' do
+  not_if "#{node.home_dir}/.rbenv/bin/rbenv global | grep #{node.ruby.version}"
+  code <<-EOH
+  #{node.home_dir}/.rbenv/bin/rbenv install #{node.ruby.version}
+  #{node.home_dir}/.rbenv/bin/rbenv global #{node.ruby.version}
+  #{node.home_dir}/.rbenv/bin/rbenv rehash
+  EOH
+end
+
+gem_package 'bundler' do
+  action :install
+end
