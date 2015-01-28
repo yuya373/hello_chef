1e99a188e9fef488ee57cd06dfac52e81ad5c884
Yuya Minami <yuya373@me.com>
2015-01-29 01:06:32 +0900

    add git recipe

---
+++ recipes/git.rb
@@ -0,0 +1,11 @@
+package 'python-software-properties'
+
+bash 'add repositry' do
+  code 'sudo add-apt-repository ppa:git-core/ppa'
+end
+
+bash 'apt-get update' do
+  code 'sudo apt-get update'
+end
+
+package 'git'
