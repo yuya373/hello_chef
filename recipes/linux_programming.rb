c1d7f6bf5d2321cdd78248672bd60b2400666408
Yuya Minami <yuya373@me.com>
2015-02-15 09:34:27 +0900

    modify linux_programming recipe

--- recipes/linux_programming.rb
+++ recipes/linux_programming.rb
@@ -1,7 +1,14 @@
 # ふつうのLinuxプログラミングのためのレポジトリ
 
+directory "#{node.home_dir}/linux_programming" do
+  user node.user
+  group node.group
+  mode 0755
+end
+
 git "#{node.home_dir}/linux_programming" do
   user node.user
+  group node.group
   repository 'git@github.com:yuya373/linux_programming.git'
 end
 
